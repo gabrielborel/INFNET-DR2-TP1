2dae85e1fcb1d6bc551ce7dc8d4834d51d7d72b4
Gabriel Borel <biel_borel@hotmail.com>
2025-03-26 18:45:21 -0300

    DR2-TP1 Exercício 2

--- src/main/java/org/INFNET/TP1/Main.java
+++ src/main/java/org/INFNET/TP1/Main.java
@@ -1,7 +1,43 @@
 package org.INFNET.TP1;
 
+class Processador {
+
+    public void processar(String dado) {
+        boolean dadoExiste = this.verificaValidade(dado);
+        if (!dadoExiste) {
+            System.out.println("Dado inválido.");
+            return;
+        }
+
+        boolean dadoTamanhoValido = this.verificaTamanho(dado);
+        if (!dadoTamanhoValido) {
+            System.out.println("Dado muito curto.");
+            return;
+        }
+
+        System.out.println("Dado válido: " + dado);
+    }
+
+    private boolean verificaValidade(String dado) {
+        return !dado.isEmpty();
+    }
+
+    private boolean verificaTamanho(String dado) {
+        int TAMANHO_MINIMO = 10;
+        return dado.length() > TAMANHO_MINIMO;
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Processador processador = new Processador();
+        processador.processar("Dado 1");
+        processador.processar("Dado 2");
+        processador.processar("Dado 3");
+        processador.processar("Dado 4");
+        processador.processar("Dado 567890");
+        processador.processar("Dado 56789012313");
+        processador.processar("Dado 567890423424");
+        processador.processar("Dado 56789034535345");
     }
 }
